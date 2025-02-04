47f7eea0927303b806b2f77abf6c8536849d760a
João R. <joaopedrorodrigueslc@gmail.com>
2025-02-03 21:51:34 -0300

    basic architecture of a shell: command interpretation and loop.

---
+++ concha.rs
@@ -0,0 +1,20 @@
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
+use std::process::Command;
+
+fn main() {
+    loop {
+        print!("$ ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        child.wait();
+    }
+}
